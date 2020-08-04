f03aefca1b8e9a7ffaf73eaeecee7a5cb2dc8f5f
Maverick Tesini <mavericktesini@gmail.com>
2020-08-04 02:08:32 -0400

    Add local state comparison

--- reduxa/src/App.tsx
+++ reduxa/src/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import Counter from './containers/Counter';
 import './App.css';
-import { selectCount, increment, decrement, incrementByAmount, incrementAsync, decrementAsync } from './redux/counterSlice';
-
-const buttonStyle = {
-  padding: '1rem 2rem',
-  margin: '0.25rem',
-}
 
 function App() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-
   return (
     <div className="App">
-      <header className="App-header">
-        Testing Only
-      <p>Count is: {count}</p>
-        <button type="button" style={buttonStyle} onClick={() => dispatch(increment())}>ADD</button>
-        <button type="button" style={buttonStyle} onClick={() => dispatch(incrementByAmount(2))}>ADD 2</button>
-        <button type="button" style={buttonStyle} onClick={() => dispatch(incrementAsync(10))}>Delayed ADD 10</button>
-        <button type="button" style={buttonStyle} onClick={() => dispatch(decrement())}>SUBTRACT</button>
-        <button type="button" style={buttonStyle} onClick={() => dispatch(decrementAsync(10))}>Delayed Reduce 10</button>
-      </header>
+      {/* <header className="App-header">
+        Testing Only */}
+      <Counter />
+      {/* </header> */}
     </div >
   );
 }
--- reduxa/src/containers/Counter.tsx
+++ reduxa/src/containers/Counter.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCount, incrementByAmount, incrementAsync, decrement, decrementAsync, increment } from '../redux/counterSlice';
+
+const buttonStyle = {
+  padding: '1rem 2rem',
+  margin: '0.25rem',
+}
 
 const Counter = () => {
   // State: a counter value
   const [counter, setCounter] = useState(0)
+  // selecting global state
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
 
-  // Action: code that causes an update to the state when something happens
-  const increment = () => {
+  // local state handlers
+  const localIncrement = () => {
+    setCounter(prevCounter => prevCounter + 1)
+  }
+  const localDecrement = () => {
     setCounter(prevCounter => prevCounter + 1)
   }
 
   // View: the UI definition
   return (
-    <div>
-      Value: {counter} <button onClick={increment}>Increment</button>
+    <div id="counter">
+      <p>Global Count is: {count}</p>
+      <p>Global State Controls</p>
+      <button type="button" style={buttonStyle} onClick={() => dispatch(increment())}>ADD</button>
+      <button type="button" style={buttonStyle} onClick={() => dispatch(incrementByAmount(2))}>ADD 2</button>
+      <button type="button" style={buttonStyle} onClick={() => dispatch(incrementAsync(10))}>Delayed ADD 10</button>
+      <button type="button" style={buttonStyle} onClick={() => dispatch(decrement())}>SUBTRACT</button>
+      <button type="button" style={buttonStyle} onClick={() => dispatch(decrementAsync(10))}>Delayed Reduce 10</button>
+
+      <hr style={{ border: '1px solid black' }} />
+      <p>Local Value is: {counter}</p>
+      <p>Local State Controls</p>
+      <button type="button" style={buttonStyle} onClick={() => localIncrement()}>Increase Local Value</button>
+      <button type="button" style={buttonStyle} onClick={() => localDecrement()}>Reduce Local Value</button>
     </div>
   )
 }
+
+export default Counter;
