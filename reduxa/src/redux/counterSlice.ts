0a87875c74c6628396f4a0c85dd84843a367cd93
Maverick Tesini <mavericktesini@gmail.com>
2020-08-04 15:14:55 -0400

    change tsx to ts for non component files, re redux

--- reduxa/src/redux/counterSlice.ts
+++ reduxa/src/redux/counterSlice.ts
@@ -26,7 +26,7 @@ export const { increment, decrement, incrementByAmount, decrementByAmount } = sl
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount: number) => (dispatch: AppDispatch) => { // ! what is standard type for dispatch? custom per or app wide?
+export const incrementAsync = (amount: number) => (dispatch: AppDispatch) => { // ? what is standard type for dispatch? custom per or app wide?
   setTimeout(() => {
     dispatch(incrementByAmount(amount));
   }, 1000);
